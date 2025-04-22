fcb8aae9d8b65f7b65864fd07b951cd408b411f2
sabirband <sabirband@gmail.com>
2025-04-22 12:59:09 +0300

    ok

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,13 +1,43 @@
 AUTHOR = 'Sabir'
-SITENAME = 'NOTELIVE | Юридичний блог майбутнього'
+SITENAME = 'NOTELIVE'
 SITEURL = 'https://blog.notelive-shvalyuk.com'
-SITESUBTITLE = 'Цифрова грамотність. Спадщина. Майбутнє.'
+SITESUBTITLE = 'Юридичний блог майбутнього: Цифрова грамотність, Спадщина, Майбутнє.'
+SITETITLE = 'ТЕСТОВЫЙ ЗАГОЛОВОК NOTELIVE'
 PATH = "content"
-THEME = 'themes/pelican-themes/Flex'
+
 TIMEZONE = 'Europe/Kyiv'
 
 DEFAULT_LANG = 'uk'
 OUTPUT_PATH = 'output/'
+
+THEME = 'themes/pelican-themes/Flex'
+# publishconf.py
+
+# ... (інші імпорти та налаштування, успадковані з pelicanconf - залишаються) ...
+
+JINJA_ENVIRONMENT = {
+    'extensions': [
+        'jinja2.ext.loopcontrols',
+    ]
+}
+
+# ... (STATIC_PATHS, EXTRA_PATH_METADATA та інші налаштування publishconf.py - залишаються) ...
+
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['sitemap', 'neighbors', 'related_posts']
+STATIC_PATHS = ['images', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
+SITELOGO_TEXT = 'Notelive'
+SITELOGO_TEXT_COLOR = '#FFD700'
+SITEMENU = [('Розлучення', '/розлучення.html'), ('Аліменти', '/аліменти.html'), ('Спадщина', '/спадщина.html'), ('Шлюб', '/шлюб.html'), ('Цифрові активи', '/цифрові-активи.html'), ('Бізнес і родина', '/бізнес-і-родина.html'), ('Криптоправо', '/криптоправо.html'), ('Право майбутнього', '/право-майбутнього.html'), ('Юридична гігієна', '/юридична-гігієна.html'), ('Форма', '/форма.html'), ('Звернутись', 'https://notelive.ua')]
+THEME_COLOR = '#000000'
+THEME_TEXT_COLOR = '#000000'
+THEME_ACCENT_COLOR = '#FFD700'
+SUMMARY_MAX_LENGTH = 40
+
 
 ARTICLE_URL = '{category}/{slug}.html'
 ARTICLE_SAVE_AS = '{category}/{slug}.html'
